bb27410670ff4a9a45fdef8c652db72e6813c30f
Adrián Juárez <comecacahuates@yahoo.com>
2023-09-28 15:36:57 -0600

    Test parseInputString function
    Co-authored-by: lorenzzz0 <lorenzzz0@users.noreply.github.com>

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,30 @@
 import { describe, it, expect } from '@jest/globals';
-import { add } from '../src/index';
+import { add, parseInputString } from '../src/index';
 
 describe('add', () => {
+  describe('parsing input string', () => {
+    it('should return default delimiter if no custom delimiter is specified', () => {
+      const { delimiter } = parseInputString('1,2');
+      console.log(delimiter);
+      expect(delimiter).toEqual(/,|\n/);
+    });
+
+    it('should return custom delimiter if specified', () => {
+      const { delimiter } = parseInputString('//;\n1;2');
+      expect(delimiter).toEqual(';');
+    });
+
+    it('should return numbers string if no custom delimiter is specified', () => {
+      const { numbersString } = parseInputString('1,2');
+      expect(numbersString).toEqual('1,2');
+    });
+
+    it('should return numbers string if custom delimiter is specified', () => {
+      const { numbersString } = parseInputString('//;\n1;2');
+      expect(numbersString).toEqual('1;2');
+    });
+  });
+
   it('should return 0 for empty string', () => {
     expect(add('')).toBe(0);
   });
