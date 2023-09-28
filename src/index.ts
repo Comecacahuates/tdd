f9512d200af0444beaacb3b46631b3cdc701c359
Adrián Juárez <comecacahuates@yahoo.com>
2023-09-28 15:36:36 -0600

    add parseInputString function
    Co-authored-by: lorenzzz0 <lorenzzz0@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
-export function add(inputString: string): number {
-  let result: number;
-
-  let numbersString = inputString;
+export function parseInputString(inputString: string) {
   let delimiter: string | RegExp = /,|\n/;
+  let numbersString = inputString;
 
   if (inputString.startsWith('//')) {
     const lines = inputString.split('\n');
     delimiter = lines[0]!.substring(2);
     numbersString = lines[1]!;
   }
+
+  return { delimiter, numbersString };
+}
+
+export function add(inputString: string): number {
+  let result: number;
+
+  const { delimiter, numbersString } = parseInputString(inputString);
 
   if (numbersString === '') {
     result = 0;
