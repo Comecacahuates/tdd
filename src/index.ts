2779a9f34bd3bde1205e0585ec7eaff32b9ccc21
Adrián Juárez <comecacahuates@yahoo.com>
2023-09-28 15:20:06 -0600

    Handle custom delimiter
    Co-authored-by: lorenzzz0 <lorenzzz0@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
-export function add(numbersString: string): number {
+export function add(inputString: string): number {
   let result: number;
+
+  let numbersString = inputString;
+  let delimiter: string | RegExp = /,|\n/;
+
+  if (inputString.startsWith('//')) {
+    const lines = inputString.split('\n');
+    delimiter = lines[0]!.substring(2);
+    numbersString = lines[1]!;
+  }
 
   if (numbersString === '') {
     result = 0;
   } else {
-    const numbers = numbersString.split(/,|\n/).map(Number);
+    const numbers = numbersString.split(delimiter).map(Number);
     result = numbers.reduce((sum, number) => sum + number);
   }
 
